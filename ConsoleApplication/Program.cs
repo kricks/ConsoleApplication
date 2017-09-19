10789aa996696434a75640383d552e1a2df9a253
asus <asus@DESKTOP-U3D379Q>
2017-09-19 08:25:57 -0600

    Program.cs Name and Christmas date stuff

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var myname = " Katie Ricks";
+            DateTime today = DateTime.Now;
+            DateTime christmas = new DateTime(2017, 12, 25);
+            TimeSpan difference = christmas - today;
+
+            Console.WriteLine($"Hello World!");
+            Console.WriteLine($"Katie Ricks");
+            Console.WriteLine($"Rexburg, Idaho");
+            Console.WriteLine($"Hello " + $"{myname}!");
+            Console.WriteLine($"The current date is: " + today);
+            Console.WriteLine($"Time until Christmas: " + difference.ToString("dd") + " days!");
+
+
+            //Program from Book
+            Console.WriteLine($"\n");
+            Console.WriteLine("Please Enter Width of Window in Meters.");
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+
+            Console.WriteLine("Please Enter Height of Window in Meters.");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet");
+
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+
+            Console.ReadLine();
         }
     }
 }
