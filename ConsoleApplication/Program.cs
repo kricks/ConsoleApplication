a61ffb2086dbcfbc04ae14b5310cc4644e031412
Katie Ricks <kricks12052@byui.edu>
2017-09-19 09:09:30 -0600

    mooo

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApplication
             DateTime today = DateTime.Now;
             DateTime christmas = new DateTime(2017, 12, 25);
             TimeSpan difference = christmas - today;
+            
             //test
-
             Console.WriteLine($"Hello World!");
             Console.WriteLine($"Katie Ricks");
             Console.WriteLine($"Rexburg, Idaho");
